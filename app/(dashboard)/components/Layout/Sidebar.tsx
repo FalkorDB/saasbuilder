d3dcd6ee1ac892cbe1636226a188d5117a8bf6a3
Cargo Audit <zimber.dudi@gmail.com>
2026-08-17 15:33:27 +0300

    fixed links to docs

--- app/(dashboard)/components/Layout/Sidebar.tsx
+++ app/(dashboard)/components/Layout/Sidebar.tsx
@@ -29,7 +29,7 @@ import {
 import APIDocsIcon from "components/Icons/SideNavbar/APIDocs/APIDocsIcon";
 import DashboardNavIcon from "components/Icons/SideNavbar/Dashboard/Dashboard";
 import DeveloperDocsIcon from "components/Icons/SideNavbar/DeveloperDocs/DeveloperDocsIcon";
-import DownloadCLIIcon from "components/Icons/SideNavbar/DownloadCLI/DownloadCLIIcon";
+// import DownloadCLIIcon from "components/Icons/SideNavbar/DownloadCLI/DownloadCLIIcon";
 import FileLockIcon from "components/Icons/SideNavbar/FileLock/FileLockIcon";
 import PricingIcon from "components/Icons/SideNavbar/Pricing/PricingIcon";
 import ReleaseHistoryIcon from "components/Icons/SideNavbar/ReleaseHistory/ReleaseHistoryIcon";
@@ -149,7 +149,7 @@ const Sidebar = () => {
   const currentPath = usePathname();
   const { serviceOfferings, subscriptions } = useGlobalData();
   const [isOverlayOpen, setIsOverlayOpen] = useState(false);
-  const [overlayType, setOverlayType] = useState<Overlay>("plan-details");
+  const [overlayType, _] = useState<Overlay>("plan-details");
   const [expandedMenus, setExpandedMenus] = useState({
     Deployments: false,
     "Governance Hub": false,
@@ -217,39 +217,28 @@ const Sidebar = () => {
         name: "API Documentation",
         icon: APIDocsIcon,
         onClick: () => {
-          window.open("https://docs.falkordb.com/cloud", "_blank");
-        },
-      },
-      {
-        name: "Download CLI",
-        icon: DownloadCLIIcon,
-        onClick: () => {
-          setIsOverlayOpen(true);
-          setOverlayType("download-cli");
+          window.open("https://docs.falkordb.com/cloud/api-reference/introduction", "_blank");
         },
       },
       {
         name: "Support",
         icon: SupportIcon,
         onClick: () => {
-          setIsOverlayOpen(true);
-          setOverlayType("support");
+          window.open("https://support.falkordb.com/", "_blank");
         },
       },
       {
         name: "Pricing",
         icon: PricingIcon,
         onClick: () => {
-          setIsOverlayOpen(true);
-          setOverlayType("pricing");
+          window.open("https://docs.falkordb.com/cloud/tiers/pro", "_blank");
         },
       },
       {
         name: "Documentation",
         icon: DeveloperDocsIcon,
         onClick: () => {
-          setIsOverlayOpen(true);
-          setOverlayType("documentation");
+          window.open("https://docs.falkordb.com/cloud", "_blank");
         },
       },
     ],
--- src/components/APIDocumentation/APIDocumentation.tsx
+++ src/components/APIDocumentation/APIDocumentation.tsx
@@ -11,7 +11,7 @@ const APIDocumentation: React.FC<APIDocumentationProps> = ({}) => {
     <CardWithTitle title="API Documentation" style={{ minHeight: "500px" }}>
       <Button
         onClick={() => {
-          window.open("https://docs.falkordb.com/cloud", "_blank");
+          window.open("https://docs.falkordb.com/cloud/api-reference/introduction", "_blank");
         }}
       >
         Open API Documentation
